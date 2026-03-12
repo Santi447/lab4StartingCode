981919df1bbe536ff2f1ec145c0cd0491e0f9027
Santiago pabon <santiago.pabon72@gmail.com>
2026-03-11 21:30:01 -0600

    initial commit

--- src/exercise1/MyAList.java
+++ src/exercise1/MyAList.java
@@ -6,7 +6,19 @@ import java.util.Iterator;
 public class MyAList<E> implements Iterable<E>
 {
 	private ArrayList<E> list;
-
+	
+	public MyAList() {
+	  list = new ArrayList<E>();
+	}
+	
+	public void add(E item) {
+		list.add(item);
+	}
+	public void addAll(MyAList<E> items) {
+		for(E item : items) {
+			list.add(item);
+		}
+	}
 	@Override
 	public Iterator<E> iterator()
 	{
