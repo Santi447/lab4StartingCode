6dc4a86811d4ada0eb8d22af6d306af2d35eb2e8
Santiago pabon <santiago.pabon72@gmail.com>
2026-03-17 10:51:02 -0600

    created method signature for exercise 2

--- src/exercise2/MyDList.java
+++ src/exercise2/MyDList.java
@@ -6,5 +6,38 @@ public class MyDList<E>
 	private MyNode<E> head;
 	private MyNode<E> tail;
 	private int size;
-
+	
+	//Constructor
+	public MyDList() {
+		head = null;
+		tail = null;
+		size = 0;
+	}
+	
+	public MyNode<E> getHead(){
+		return this.head;
+	}
+	
+	public void addFirst(E item) {
+		
+	}
+	
+	public void addLast(E item) {
+		
+	}
+	
+	public E removeFirst() {
+		return null;
+	}
+	
+	public E removeLast() {
+		return null;
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	public boolean isEmpty() {
+		return false;
+	}
 }
