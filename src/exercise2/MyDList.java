1e82298696ef4bd5b4228230941de6e078bbb8e3
Santiago pabon <santiago.pabon72@gmail.com>
2026-03-18 18:50:38 -0600

    lab 4 completed

--- src/exercise2/MyDList.java
+++ src/exercise2/MyDList.java
@@ -19,25 +19,80 @@ public class MyDList<E>
 	}
 	
 	public void addFirst(E item) {
-		
+		MyNode<E> newNode = new MyNode<>(item);
+		if(isEmpty()) {
+
+		head = newNode;
+		tail = newNode;
+		}
+		else {
+		newNode.setNext(head);
+		head.setPrev(newNode);
+		head = newNode;
+		}
+		size++;
 	}
 	
 	public void addLast(E item) {
+		MyNode<E> newNode = new MyNode<>(item);	
+		
+		if(isEmpty()) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+		tail.setNext(newNode);
+		newNode.setPrev(tail);
+		tail = newNode;
+		}
+		size++;
 		
 	}
 	
 	public E removeFirst() {
-		return null;
+		if(isEmpty()) {
+			return null;
+		}
+		else {
+		E oldHead = head.getElement();
+		if (size == 1) {
+			head = null;
+			tail = null;
+		}
+		else {
+			head.getNext().setPrev(null);
+			head = head.getNext();
+		}
+		size--;
+		return oldHead;
+		}
 	}
 	
 	public E removeLast() {
-		return null;
+		if(isEmpty()) {
+			return null;
+		}
+		else {
+		E oldTail = tail.getElement();
+		if(size == 1) {
+			head = null;
+			tail = null;
+		}
+		else {
+			tail.getPrev().setNext(null);
+			tail = tail.getPrev();
+			
+		}
+		size --;
+		return oldTail;
+		}
 	}
 	
 	public int size() {
 		return this.size;
 	}
 	public boolean isEmpty() {
+		if (size == 0) return true;	
 		return false;
 	}
 }
--- src/exercise2/MyNode.java
+++ src/exercise2/MyNode.java
@@ -6,5 +6,34 @@ public class MyNode<E>
 	private E element;
 	private MyNode<E> next;
 	private MyNode<E> prev;
+	
+	public MyNode(E element){
+		
+		this.element = element;
+		this.next = null;
+		this.prev = null;
+	}
+	
+	public E getElement() {
+		return this.element;
+	}
+	
+	public MyNode<E> getNext() {
+		return this.next;
+	}
+	
+	public MyNode<E> getPrev(){
+		return this.prev;
+	}
+	
+	public void setElement(E element) {
+		this.element = element;
+	}
+	public void setNext(MyNode<E> newNode) {
+		this.next = newNode;
+	}
+	public void setPrev(MyNode<E> newNode) {
+		this.prev = newNode;
+	}
 
 }
